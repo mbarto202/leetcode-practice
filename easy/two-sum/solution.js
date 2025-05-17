86be77c419820700039924573cf5d6d3c31038ff
Michael <michael.d.barto@gmail.com>
2025-05-16 22:09:48 -0400

    twosum

--- easy/two-sum/solution.js
+++ easy/two-sum/solution.js
@@ -1,3 +1,4 @@
+// function
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
@@ -7,3 +8,8 @@ var twoSum = function (nums, target) {
     }
   }
 };
+
+// test
+const test = twoSum([1, 2, 3], 3);
+
+console.log("test:", test);
