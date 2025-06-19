263d83439abebc18cf42716ae5a68141ad969154
Michael <michael.d.barto@gmail.com>
2025-06-19 10:57:23 -0400

    sliding window pattern

---
+++ patterns/sliding_window.js
@@ -0,0 +1,7 @@
+/* When to Use:
+    - Problems asking for subarrays, substrings, or ranges
+    - Fixed size(like "max sum f k-length subarray")
+    - variable size (like "longest substring with X condition")
+*/
+
+// Sliding Window Pattern
