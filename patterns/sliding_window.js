754ad60eb75317f38386a36c4ffd99b6cd30848c
Michael <michael.d.barto@gmail.com>
2025-06-20 22:03:17 -0400

    sliding window pattern

--- patterns/sliding_window.js
+++ patterns/sliding_window.js
@@ -1,7 +1,29 @@
-/* When to Use:
+// Sliding Window
+
+let left = 0;
+
+for (let right = 0; right < arr.length; right++){
+    // expand the window
+
+    while(window violates condition) {
+        // shrink window from left
+
+        left++
+    }
+
+    // track max/min/etc..
+}
+
+/*  ------------------------------------------------------------
+
+    When to Use:
     - Problems asking for subarrays, substrings, or ranges
     - Fixed size(like "max sum f k-length subarray")
     - variable size (like "longest substring with X condition")
-*/
 
-// Sliding Window Pattern
+    Practice Problems:
+    - Maximum Sum of Subarray of Size K (fixed)
+    - Longest Substring Without Repeating Characters (variable)
+    - Permutation in String
+
+*/
