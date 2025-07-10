f20f76ddbfd947f4239c886c8b2bf365996857cb
Michael <michael.d.barto@gmail.com>
2025-07-10 11:06:56 -0400

    working on two pointer

--- patterns/two_pointer.js
+++ patterns/two_pointer.js
@@ -1,1 +1,25 @@
 // Two-Pointer
+
+let left = 0;
+let right = Array.length - 1;
+
+while (left < right) {
+  const sum = arr[left] + arr[right];
+
+  if (sum === target) return [left, right];
+  else if (sum < target) left++;
+  else right--;
+}
+
+/*  ------------------------------------------------------------
+
+    When to Use:
+    - Sorted arrays or linked lists
+    - Looking for pairs, duplicates, or intersections
+
+    Practice Problems:
+    - Two Sum 2(sorted array)
+    - Container With Most Water
+    - 3Sum(after sorting)
+
+*/
