8a955d00d362a4fb82396642b8ae7940a975942b
Michael <michael.d.barto@gmail.com>
2025-11-10 12:44:43 -0500

    feat: solve Two Sum problem

---
+++ javacode/array/TwoSum.java
@@ -0,0 +1,27 @@
+/*
+Leetcode 1: Two Sum
+
+Given an array of integers nums and an integer target, return indices of the 
+two numbers such that they add up to target.
+
+Example:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+*/
+
+package javacode.array;
+
+public class TwoSum {
+    public static void main(String[] args){
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    System.out.println(i+" "+j);
+                }
+            }
+        }
+    }
+}
