35b1781b8581bee0dc439423e50515b6620f4a6c
Michael <michael.d.barto@gmail.com>
2025-11-14 06:30:09 -0500

    docs: add problem description

--- javacode/array/ProductofArrayExceptSelf.java
+++ javacode/array/ProductofArrayExceptSelf.java
@@ -1,3 +1,24 @@
+/*
+Leetcode 121: Best Time to Buy and Sell Stock
+
+You are given an array prices where prices[i] is the price of a stock on the i-th day.
+
+Your goal is to choose a single day to buy one stock and choose a later day
+to sell that stock in order to maximize your profit.
+
+Return the maximum profit possible. If no profit can be made, return 0.
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy at price 1 and sell at price 6.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: No profitable transaction possible.
+*/
+
 package javacode.array;
 
 public class ProductofArrayExceptSelf {
