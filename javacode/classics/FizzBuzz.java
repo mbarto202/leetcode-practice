5fc1682d5b99238e99f3281d4688d62b29937cbb
Michael <michael.d.barto@gmail.com>
2025-11-24 23:42:43 -0500

    docs: add problem description

--- javacode/classics/FizzBuzz.java
+++ javacode/classics/FizzBuzz.java
@@ -1,3 +1,21 @@
+/*
+Classic Interview Problem: FizzBuzz
+
+Write a program that prints the numbers from 1 to n. But for multiples 
+of 3, print "Fizz" instead of the number and for the multiples of 5, print "Buzz". 
+For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+
+Example:
+Input: n = 5
+Output:
+1
+2
+Fizz
+4
+Buzz
+*/
+
+
 package javacode.classics;
 
 public class FizzBuzz {
