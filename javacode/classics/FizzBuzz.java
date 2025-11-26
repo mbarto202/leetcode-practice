58531f4b85bb96bdcb12f68af5cd283c07f447e4
Michael <michael.d.barto@gmail.com>
2025-11-25 23:10:26 -0500

    feat: solved TwoSum problem

--- javacode/classics/FizzBuzz.java
+++ javacode/classics/FizzBuzz.java
@@ -19,5 +19,20 @@ Buzz
 package javacode.classics;
 
 public class FizzBuzz {
-    
+    public static void main(String[] args){
+        int n = 15;
+
+        for(int i = 0; i <= n; i++){
+            if(i%3==0 && i%5==0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i%3==0){
+                System.out.println("Fizz");
+            }
+            else if(i%5==0){
+                System.out.println("Buzz");
+            }
+            else System.out.println(i);
+        }
+    }
 }
